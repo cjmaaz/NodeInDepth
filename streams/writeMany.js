869a8914ce89c738523c6cf5203ea980b6228f4c
Maaz Rahman <maaz@techie.com>
2026-01-08 12:57:30 +0530

    :memo: In queue with writableLength

--- streams/writeMany.js
+++ streams/writeMany.js
@@ -9,13 +9,14 @@ const testFile = resolve(__dirname, './test.txt');
 
 console.time('writeMany');
 const fileHandler = await fs.open(testFile, 'w');
-
 const stream = fileHandler.createWriteStream();
 
 console.log('HighWaterMark in Bytes: ', stream.writableHighWaterMark); // In v24.12 -> 64 KiB or 65536 Bytes
+console.log('How much in queue is filled, ready to be written: ', stream.writableLength);
 
 for (let i = 0; i < 100000; i++) {
   const buff = Buffer.from(` ${i} `, 'utf-8');
   stream.write(buff);
 }
 console.timeEnd('writeMany');
+console.log('How much in queue is filled, ready to be written: ', stream.writableLength);
